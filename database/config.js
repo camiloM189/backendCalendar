6cb03f917868c840e41df1a87d705978a082c617
camilo <jcamilomillan18912@gmail.com>
2023-03-29 09:12:39 -0500

    2

--- database/config.js
+++ database/config.js
@@ -7,8 +7,8 @@ const dbConnection = async() => {
 
         await mongoose.connect(process.env.DB_CNN, {
             socketOptions: {
-                socketTimeoutMS: 0,
-                connectionTimeout: 0
+                socketTimeoutMS: 30000,
+                connectionTimeout: 30000
               }
         });
         console.log('DB online');
